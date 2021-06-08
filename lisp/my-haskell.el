52f8f3d3007334aa3aeb5cf97b8c59ece2f57b1d
an <>
2021-06-08 12:53:17 +0800

    feature: use dante to wirte haskell

--- lisp/my-haskell.el
+++ lisp/my-haskell.el
@@ -7,20 +7,22 @@
 
 (require 'haskell-mode)
 (require 'haskell-indent)
+;;(require 'haskell-interactive-mode)
+;;(require 'haskell-process)
+(require 'dante)
 
+;;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
-;;(add-to-list 'load-path "~/.emacs.d/lisp/ghc-mod-1.11.3/elisp")
-;;(load "~/.emacs.d/lisp/ghc-mod-1.11.3/elisp/ghc.el")
 (with-eval-after-load 'highlight-indent-guides
   (add-hook 'haskell-mode-hook (lambda ()
 				 (highlight-indent-guides-mode -1))))
 
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;;(autoload 'ghc-init "ghc" nil t)
+;;(autoload 'ghc-debug "ghc" nil t)
+;;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 ;;(require 'flymake-haskell-multi)
 ;;(add-hook 'haskell-mode-hook 'flymake-haskell-multi-load)
@@ -32,13 +34,16 @@
 (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
 
 
+
 (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
 (define-key haskell-mode-map (kbd "<f5>") 'haskell-process-load-file)
 
-(add-hook 'haskell-mode-hook
-	  (lambda ()
-	    (setq-local flycheck-disabled-checkers '(haskell-stack-ghc))))
+;; (add-hook 'haskell-mode-hook
+;; 	  (lambda ()
+;; 	   (setq-local flycheck-disabled-checkers '(haskell-stack-ghc))))
 
+(add-hook 'haskell-mode-hook 'dante-mode)
+(flycheck-add-next-checker 'haskell-dante '(info . haskell-hlint))
 
 (provide 'my-haskell)
 
