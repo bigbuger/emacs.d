3e712677a1771922ca3200c67fb2718ae847a1c2
an <>
2021-12-03 21:40:16 +0800

    fix: disable python lsp flycheck

--- lisp/my-python.el
+++ lisp/my-python.el
@@ -10,7 +10,11 @@
 (require 'dap-python)
 
 (setq lsp-python-ms-auto-install-server t)
-(add-hook 'python-mode-hook #'lsp) ; or lsp-deferred
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq-local lsp-diagnostics-provider :none)
+	    (lsp)))
 
 (setq lsp-python-ms-python-executable-cmd "python3")
 
