301e9d3efedb55d3953bad210f38616d2d241aef
an <>
2021-12-30 19:11:52 +0800

    rust

--- lisp/my-rust.el
+++ lisp/my-rust.el
@@ -7,14 +7,23 @@
 ;;; Code:
 
 (require 'rust-mode)
-(require 'racer)
+;;(require 'racer)
 
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
+;;(add-hook 'rust-mode-hook #'racer-mode)
+;;(add-hook 'racer-mode-hook #'eldoc-mode)
 
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+;; (with-eval-after-load 'rust-mode
+;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+
+(setq lsp-rust-server 'rust-analyzer)
+(add-hook 'rust-mode-hook
+	  (lambda ()
+	    (lsp)))
+
+
 
 (provide 'my-rust)
+
 
 ;;; my-rust.el ends here
