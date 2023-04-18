f45e4df7e2aae3c226d6213493be0572000a2dc2
an <>
2023-04-18 18:33:36 +0800

    misc

--- init.el
+++ init.el
@@ -59,4 +59,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-function-name-face ((t (:inherit default :foreground "#9a4622")))))
+)
