fbf844ea730e41fbcc67691bb9754cf1b9ec36c2
an <>
2023-09-20 23:45:36 +0800

    latex config

--- lisp/my-latex.el
+++ lisp/my-latex.el
@@ -6,7 +6,27 @@
 ;;; Code:
 
 
-(load "auctex-autoloads")
+(use-package tex
+	:ensure auctex
+	;; 若使用 straight, 注释前一行, 并取消下一行注释:
+	;; :straight auctex
+	:custom
+	(TeX-parse-self t) ; 自动解析 tex 文件
+	(TeX-PDF-mode t)
+	(TeX-DVI-via-PDFTeX t)
+	:config
+	(setq-default TeX-master t) ; 默认询问主文件
+	(add-hook 'LaTeX-mode-hook 'turn-on-reftex))
+
+(use-package cdlatex
+  :after tex ; 保证 cdlatex 在 auctex 之后加载
+  :ensure t
+  :config
+  (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex))
+
+
+
+
 (require 'xenops)
 
 
