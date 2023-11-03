b56575bd7ee7475faa6b7827e57a5669cb7f2e17
an <>
2023-11-03 22:46:55 +0800

    extract init-version-control from global.el

--- lisp/common/global.el
+++ lisp/common/global.el
@@ -185,58 +185,6 @@
 (require 'all-the-icons-ibuffer)
 (add-hook 'ibuffer-mode-hook #'all-the-icons-ibuffer-mode)
 
-
-;; magit
-(require 'magit)
-(global-set-key (kbd "C-c g") ' magit-file-dispatch)
-(global-set-key (kbd "C-c C-g") 'magit-dispatch)
-(require 'magit-todos)
-(magit-todos-mode)
-
-(setq magit-log-margin '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18))
-(setq magit-git-output-coding-system 'utf-8-unix)
-(add-to-list 'magit-process-password-prompt-regexps
-	     ".*verification code: ?$")
-
-(push '(margin
-	(margin-format    . ("%C %a %s%f"))
-	(margin-width     . 42)
-	(margin-face      . magit-blame-margin)
-	(margin-body-face . (magit-blame-dimmed)))
-      magit-blame-styles)
-
-(require 'magit-delta)
-;; (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1)))
-
-;; ediff
-(setq ediff-split-window-function 'split-window-horizontally
-      ediff-window-setup-function 'ediff-setup-windows-plain)
-(defun ediff-copy-both-to-C ()
-  (interactive)
-  (ediff-copy-diff ediff-current-difference nil 'C nil
-                   (concat
-                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
-                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
-(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
-(setq ediff-show-ancestor nil)
-
-;; diff-hl
-(require 'diff-hl)
-(global-diff-hl-mode)
-(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-(defhydra hydra-diff-hl (global-map "C-c v")
-  "vc"
-  ("n" diff-hl-next-hunk "next hunk")
-  ("p" diff-hl-previous-hunk "previous hunk")
-  ("r" diff-hl-revert-hunk "revert hunk")
-  ("q" nil "exit"))
-
-
-
-
-
 ;; dumb-jump
 (require 'dumb-jump)
 (setq dumb-jump-force-searcher 'rg)
@@ -417,9 +365,6 @@ Use a prefix argument ARG to indicate creation of a new process instead."
 	  (display-buffer xwidget-webkit-last-session-buffer)))
 (define-key prog-mode-map (kbd "C-c C-d") 'counsel-dash-at-point)
 
-
-;; gitlab
-(use-package lab :ensure t)
 
 (use-package pdf-tools
   :ensure t
---
+++ lisp/common/init-version-control.el
@@ -0,0 +1,74 @@
+;;; init-version-control.el --- 版本控制相关配置,如 git、diff
+
+
+;; magit
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(require 'magit)
+(global-set-key (kbd "C-c g") ' magit-file-dispatch)
+(global-set-key (kbd "C-c C-g") 'magit-dispatch)
+(require 'magit-todos)
+(magit-todos-mode)
+
+(setq magit-log-margin '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18))
+(setq magit-git-output-coding-system 'utf-8-unix)
+(add-to-list 'magit-process-password-prompt-regexps
+	     ".*verification code: ?$")
+
+;; 让magit blame 在左侧边显示
+(push '(margin
+	(margin-format    . ("%C %a %s%f"))
+	(margin-width     . 42)
+	(margin-face      . magit-blame-margin)
+	(margin-body-face . (magit-blame-dimmed)))
+      magit-blame-styles)
+
+(require 'magit-delta)
+;; (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1)))
+
+;; end magit
+
+
+;; ediff
+(setq ediff-split-window-function 'split-window-horizontally
+      ediff-window-setup-function 'ediff-setup-windows-plain)
+(defun ediff-copy-both-to-C ()
+  "合并时两边都保留."
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
+;; ediff 解决冲突 会显示 4 个面板，源分支、目标分支、合并结果、ancestore，不知道 ancestore 怎么用，就把他给关了
+(setq ediff-show-ancestor nil)
+
+;; end ediff
+
+;; diff-hl 侧边显示每一行的版本状态
+(require 'diff-hl)
+(global-diff-hl-mode)
+(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+(require 'hydra)
+(defhydra hydra-diff-hl (global-map "C-c v")
+  "vc"
+  ("n" diff-hl-next-hunk "next hunk")
+  ("p" diff-hl-previous-hunk "previous hunk")
+  ("r" diff-hl-revert-hunk "revert hunk")
+  ("q" nil "exit"))
+
+;; end diff-hl
+
+;; gitlab
+(use-package lab :ensure t)
+
+(provide 'init-version-control)
+
+;;; init-version-control.el ends here
