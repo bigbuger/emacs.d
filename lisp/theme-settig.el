669e77b7d92ec7e0dccc670ce8e5a4ce2e388c30
an <>
2023-11-24 23:27:39 +0800

    color setting

--- lisp/theme-settig.el
+++ lisp/theme-settig.el
@@ -15,29 +15,43 @@
 (load-theme 'spacemacs-light t)
 (custom-theme-set-faces
  'spacemacs-light
+ 
+ ;; auto highlight 高亮色
  '(ahs-face ((t (:background "#e6ebeb"))))
  '(ahs-plugin-default-face ((t (:background "#efebfb"))))
+ 
  '(aw-leading-char-face ((t (:foreground "red" :height 5.0))))
  '(diff-hl-change ((t (:background "blue3" :foreground "blue3"))))
  '(diff-hl-delete ((t (:inherit diff-removed :background "red3" :foreground "red3"))))
  '(diff-hl-insert ((t (:inherit diff-added :background "green4" :foreground "green4"))))
+
+ ;; flycheck
  '(flycheck-pos-tip-max-width 20)
  '(flycheck-error ((t (:underline "Red1"))))
  '(flycheck-info ((t (:underline (:color "#3a81c3" :style wave)))))
+ '(flycheck-warning ((t (:underline (:color "yellow4" :style wave)))))
  '(flycheck-posframe-error-face ((t (:inherit flycheck-posframe-face :foreground "VioletRed1"))))
  '(flycheck-posframe-face ((t (:inherit default :background "#efeae9"))))
- '(flycheck-warning ((t (:underline (:color "yellow4" :style wave)))))
+
+ ;; 代码高亮色修改
  '(font-lock-constant-face ((t (:foreground "#6c71c4"))))
  '(font-lock-function-name-face ((t (:inherit default :foreground "#9a4622"))))
  '(font-lock-keyword-face ((t (:inherit nil :foreground "#3a81c3" :slant normal))))
  '(font-lock-type-face ((t (:inherit default :foreground "#859900"))))
+
+ ;; lsp 当前变量高亮 
  '(lsp-face-highlight-textual ((t (:inherit highlight :background "#e6ebeb"))))
+
  '(show-paren-match ((t (:inherit bold :foreground "royal blue" :underline t))))
  '(term-color-blue ((t (:background "SkyBlue3" :foreground "SkyBlue3"))))
  '(treemacs-directory-face ((t (:inherit default))))
- ;; 设置代码块用上下边线包裹
+
+ ;; org 设置代码块用上下边线包裹
  '(org-block-begin-line ((t (:underline "#cccccc" :background "#e1e0e1"))))
- '(org-block-end-line ((t (:overline nil :underline nil :background unspecified)))))
+ '(org-block-end-line ((t (:overline nil :underline nil :background unspecified))))
+
+ ;; xeft 高亮色
+ '(xeft-inline-highlight ((t (:inherit underline :extend t :foreground "#3a81c3")))))
 
  
 (require 'doom-modeline)
