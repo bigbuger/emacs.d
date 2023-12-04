5eee6d8c586f4dc763b8e43c6a5b2e51002771bf
an <>
2023-12-04 15:00:03 +0800

    bullets

--- lisp/org-config/init-org.el
+++ lisp/org-config/init-org.el
@@ -68,6 +68,7 @@
 (use-package org-superstar
   :init
   (setq org-superstar-remove-leading-stars t)
+  (setq org-superstar-headline-bullets-list '("◉" "○" "❖" "◇" ))
   :hook
   (org-mode . org-superstar-mode))
 
