54f8e9b0bf3e02dddc9f2f769d6a99032488090b
an <>
2023-12-04 12:42:24 +0800

    smart-switch-project

--- lisp/common/init-tabbar.el
+++ lisp/common/init-tabbar.el
@@ -1,8 +1,8 @@
 ;;; init-tabbar.el --- tab bar setting
-;; 
+;;
 
 ;;; Commentary:
-;; 
+;;
 
 ;;; Code:
 
@@ -43,7 +43,7 @@
 
      ;; Is not magit buffer.
      (and (string-prefix-p "magit" name)
-          (not (file-name-extension name)))
+	  (not (file-name-extension name)))
      )))
 
 
@@ -60,7 +60,7 @@
    (cond
     ((string-prefix-p org-roam-directory (buffer-file-name))
      "Roam")
-    
+
     ((or (derived-mode-p 'eshell-mode)
 	 (derived-mode-p 'term-mode)
 	 (derived-mode-p 'shell-mode)
@@ -73,7 +73,7 @@
      (centaur-tabs-get-group-name-with-perfix "Dired")) ;; dired 模式，每个项目一个组
 
     ;; ((and (not (projectile-project-root))
-    ;; 	  (memq major-mode '(org-mode org-agenda-mode diary-mode)))
+    ;;	  (memq major-mode '(org-mode org-agenda-mode diary-mode)))
     ;;  "OrgMode")
 
     ((or (string-equal "*" (substring (buffer-name) 0 1))
@@ -138,9 +138,9 @@
 	       (same-target-check  (string-equal foreground-buffer-name target-buffer-name))
 	       (window-num         (- (length (get-buffer-window-list buffer))
 				      (if same-target-check 0 1))))
-          (if (> window-num 1)
-              (delete-window window)
-            (awesome-tabs-buffer-close-tab `,(awesome-tab-get-tab-from-event event))))))))
+	  (if (> window-num 1)
+	      (delete-window window)
+	    (awesome-tabs-buffer-close-tab `,(awesome-tab-get-tab-from-event event))))))))
 
 ;; bank the adjust color method.
 (defun awesome-tab-adjust-color-with-theme ()
@@ -151,13 +151,13 @@
   "Return a label for TAB.
 That is, a string used to represent it on the tab bar."
   (let* ((is-active-tab (awesome-tab-selected-p tab (awesome-tab-current-tabset)))
-         (tab-face (if is-active-tab 'awesome-tab-selected-face 'awesome-tab-unselected-face)))
+	 (tab-face (if is-active-tab 'awesome-tab-selected-face 'awesome-tab-unselected-face)))
     (concat
      ;; left margin
      (propertize " " 'face tab-face)
      ;; Tab icon.
      (when (and awesome-tab-display-icon
-                awesome-tab-all-the-icons-is-load-p)
+		awesome-tab-all-the-icons-is-load-p)
        (awesome-tab-icon-for-tab tab tab-face))
      ;; Tab label.
      (propertize (awesome-tab-tab-name tab)
@@ -189,27 +189,27 @@ That is, a string used to represent it on the tab bar."
   "When tab buffer's file is exists, use `all-the-icons-icon-for-file' to fetch file icon.
 Otherwise use `all-the-icons-icon-for-buffer' to fetch icon for buffer."
   (when (and awesome-tab-display-icon
-             awesome-tab-all-the-icons-is-load-p)
+	     awesome-tab-all-the-icons-is-load-p)
     (let* ((tab-buffer (car tab))
-           (tab-file (buffer-file-name tab-buffer))
-           (background (face-background face))
+	   (tab-file (buffer-file-name tab-buffer))
+	   (background (face-background face))
 	   (underline (face-attribute face :underline))
-           (icon
-            (cond
-             ;; Use `all-the-icons-icon-for-file' if current file is exists.
-             ((and
-               tab-file
-               (file-exists-p tab-file))
-              (all-the-icons-icon-for-file tab-file :v-adjust awesome-tab-icon-file-v-adjust :height awesome-tab-icon-height))
-             ;; Use `all-the-icons-icon-for-mode' for current tab buffer at last.
-             (t
-              (with-current-buffer tab-buffer
-                (all-the-icons-icon-for-mode major-mode :v-adjust awesome-tab-icon-mode-v-adjust :height awesome-tab-icon-height))))))
+	   (icon
+	    (cond
+	     ;; Use `all-the-icons-icon-for-file' if current file is exists.
+	     ((and
+	       tab-file
+	       (file-exists-p tab-file))
+	      (all-the-icons-icon-for-file tab-file :v-adjust awesome-tab-icon-file-v-adjust :height awesome-tab-icon-height))
+	     ;; Use `all-the-icons-icon-for-mode' for current tab buffer at last.
+	     (t
+	      (with-current-buffer tab-buffer
+		(all-the-icons-icon-for-mode major-mode :v-adjust awesome-tab-icon-mode-v-adjust :height awesome-tab-icon-height))))))
       (when (and icon
-                 ;; `get-text-property' need icon is string type.
-                 (stringp icon))
-        ;; Thanks ema2159 for code block ;)
-        (propertize icon 'face `(:inherit ,(get-text-property 0 'face icon) :background ,background :underline ,underline))))))
+		 ;; `get-text-property' need icon is string type.
+		 (stringp icon))
+	;; Thanks ema2159 for code block ;)
+	(propertize icon 'face `(:inherit ,(get-text-property 0 'face icon) :background ,background :underline ,underline))))))
 
 (define-key awesome-tab-mode-map (kbd "s-t 1")
 	    'awesome-tab-kill-other-buffers-in-current-group)
@@ -219,6 +219,18 @@ Otherwise use `all-the-icons-icon-for-buffer' to fetch icon for buffer."
 (add-hook 'after-init-hook
 	  (awesome-tab-mode t))
 
+
+;; hack, ueing awesome-tab-switch-group to let projectile switch to opened project buffer.
+(defun smart-switch-project ()
+  "Open latest edited buffer when switched the exist project, find files when switched to a new project."
+  (let* ((exists-projects (awesome-tab-get-groups))
+	 (project-root (projectile-project-root))
+	 (tab-group-name (format "Project: %s" project-root)))
+    (if (member tab-group-name exists-projects)
+	(awesome-tab-switch-group tab-group-name)
+      (projectile-find-file))))
+(with-eval-after-load 'projectile
+  (setq projectile-switch-project-action 'smart-switch-project))
 
 (provide 'init-tabbar)
 
