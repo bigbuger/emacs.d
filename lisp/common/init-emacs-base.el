588c9c9359fe527e1a86ee5c95f01f3b551433db
an <>
2023-12-07 23:19:26 +0800

    don not need too many backup

--- lisp/common/init-emacs-base.el
+++ lisp/common/init-emacs-base.el
@@ -12,8 +12,6 @@
       backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 20   ; how many of the newest versions to keep
-      kept-old-versions 5    ; and how many of the old
       )
 
 ;; 记录文件上次打开的位置
