18be7f46c0b732927265582b713dd47605d07c20
an <>
2023-12-20 20:33:48 +0800

    try gptel

--- lisp/common/init-z-ellama.el
+++ lisp/common/init-z-ellama.el
@@ -12,5 +12,14 @@
   (require 'llm-ollama))
 
 
+(use-package gptel
+  :init
+  (setq-default gptel-model "zephyr:latest"
+		gptel-backend (gptel-make-ollama
+		 "Ollama"                               ;Any name of your choosing
+		 :host "localhost:11434"                ;Where it's running
+		 :models '("zephyr:latest")            ;Installed models
+		 :stream t)))
+
 
 ;;; init-ellama.el ends here
