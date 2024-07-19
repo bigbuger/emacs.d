58d7fe4132fbea6d0bcf43d6d757d4791486a195
an <>
2024-07-19 17:54:35 +0800

    glpk keywords and flycheck filter

--- lisp/lang/init-glpk.el
+++ lisp/lang/init-glpk.el
@@ -11,8 +11,15 @@
     (flycheck-define-checker gmpl
       "A gplk/gmpl syntax checker."
       :command ("glpsol" "--check" "-m" source)
+      :error-filter (lambda (errors)
+		      (seq-filter (lambda (err)
+				    ;; (message (flycheck-error-message err))
+				    (not (string-prefix-p "no value for"
+							  (string-trim (flycheck-error-message err)))))
+				  errors))
       :error-patterns
       (
+       (warning line-start (file-name) ":" line ": warning:" (message)  line-end)
        (error line-start (file-name) ":" line ":" (message)  line-end))
       :modes gmpl-mode)
     (add-to-list 'flycheck-checkers 'gmpl))
@@ -20,25 +27,33 @@
   :config
   (with-eval-after-load 'company-keywords
     (add-to-list 'company-keywords-alist
-		 '(gmpl-mode "and" "else" "mod" "union"
+		 '(gmpl-mode "set" "param" "var"
+			     "maximize" "minimize"
+			     "s.t." "subject to" "subj to"
+			     
+			     "and" "else" "mod" "union"
 			     "by" "if" "not" "within"
 			     "cross" "in" "or"
 			     "diff" "inter" "symdiff"
 			     "div" "less" "then"
-			     "maximize" "minimize"
+			     
 			     "dimen" "default" "integer" "binary" "symbolic"
 			     "for" "check" "table" "IN" "OUT"
+			     
 			     "data" "end" "solve"
-			     "s.t." "subject to" "subj to"
+			     
 			     "sum" "prod" "min" "max" "setof" "forall" "exists"
+
 			     "abs" "atan" "card" "ceil" "cos"
 			     "exp" "floor" "gmtime" "length"
 			     "log" "log10" "max" "min" "round"
 			     "sin" "sqrt" "str2time" "trunc"
 			     "Irand224" "Uniform01" "Uniform"
 			     "Normal01" "Normal"
+			     
 			     "substr" "time2str"
-			     "printf")))
+			     
+			     "display" "printf")))
   )
 
 (defun org-babel-execute:gmpl (body params)
