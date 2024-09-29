aa8be15a28e2b4024240e97168493f391248fa9f
an <>
2024-09-29 12:50:29 +0800

    lean4

---
+++ lisp/lang/init-lean4.el
@@ -0,0 +1,15 @@
+;;; init-lean4.el --- lean4
+
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(use-package lean4-mode
+  :load-path "~/.emacs.d/lisp/libs/lilean4-mode"
+  )
+
+(provide 'init-lean4)
+
+;;; init-lean4.el ends here
