febc2fba10838770f471188f4b6e54f2c8cbbbd4
an <>
2024-09-30 15:26:29 +0800

    lean4 keywords completion

--- lisp/lang/init-lean4.el
+++ lisp/lang/init-lean4.el
@@ -8,6 +8,18 @@
 
 (use-package lean4-mode
   :load-path "~/.emacs.d/lisp/libs/lilean4-mode"
+
+  :config
+  (with-eval-after-load 'company-keywords
+    (add-to-list 'company-keywords-alist
+		 `(lean4-mode ,@lean4-keywords1)))
+  (add-hook 'lean4-mode-hook
+	    (lambda ()
+	      (setq-local company-backends
+			  '((company-capf company-keywords company-yasnippet)
+			    company-files
+			    company-dabbrev-code
+			    company-dabbrev))))
   )
 
 (provide 'init-lean4)
