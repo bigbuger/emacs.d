1fd65bbde2ae9f7bff61764548c8ebcf8250648f
an <>
2025-04-01 17:32:26 +0800

    gc setting

--- early-init.el
+++ early-init.el
@@ -1,1 +1,7 @@
 (setq native-comp-jit-compilation nil)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold (* 50 1024 1024))
+
+(provide 'early-init-file)
