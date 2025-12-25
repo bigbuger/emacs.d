439a7e2729948a94ee612b0f30d3b97692540d13
an <>
2025-12-25 15:10:27 +0800

    try kmacro-x

--- lisp/common/init-0-editor-enhance.el
+++ lisp/common/init-0-editor-enhance.el
@@ -144,11 +144,12 @@ ARG is pass to `sp-end-of-sexp'"
   ;; :init (kmacro-x-atomic-undo-mode 1)
   :bind (("C-<" . kmacro-x-mc-mark-previous)
          ("C->" . kmacro-x-mc-mark-next)
+	 ("C-S-<mouse-1>" . kmacro-x-mc-mark-at-click)
 	 :map kmacro-x-mc-mode-map
 	 ("C-<return>" . kmacro-x-mc-apply))
   :config
   (setq kmacro-x-mc-live-preview t)
-  (unbind-key (kbd "<return>") kmacro-x-mc-mode-map))
+  (unbind-key (kbd "<return>") 'kmacro-x-mc-mode-map))
 
 ;; dmacro 动态生成键盘宏
 ;; 重复一套操作两次后直接用 `C-S-e' 后就直接调用
